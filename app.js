917344d5a8b6f538ddcad7694398b283f5fa4324
Gideon AMOUSSOU-CHOUH <hged04@gmail.com>
2022-01-31 17:53:49 +0100

    Updated website files

--- app.js
+++ app.js
@@ -15,7 +15,56 @@ const countdownfunction = setInterval(function () {
   if (diff < 0) {
     clockdiv.style.display = "none";
     clearInterval(countdownfunction);
-  } else {
+  } else {//Countdown Timer
+    const clockdiv = document.getElementById("countdown");
+    const countDownTime = new Date(
+      clockdiv.getAttribute("data-date")
+    ).getTime();
+    
+    const countdownfunction = setInterval(function () {
+      const now = new Date().getTime();
+      const diff = countDownTime - now;
+      const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+      const hours = Math.floor(diff % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
+      const minutes = Math.floor(diff % (1000 * 60 * 60) / (1000 * 60));
+      const seconds = Math.floor(diff % (1000 * 60) / 1000);
+    
+      if (diff < 0) {
+        clockdiv.style.display = "none";
+        clearInterval(countdownfunction);
+      } else {
+        clockdiv.querySelector(".days").innerHTML = days + " :";
+        clockdiv.querySelector(".hours").innerHTML = hours + " :";
+        clockdiv.querySelector(".minutes").innerHTML = minutes + " :";
+        clockdiv.querySelector(".seconds").innerHTML = seconds;
+      }
+    }, 1000);
+    
+    
+    //Countdown Timer
+    const clockdivs = document.getElementById("countdowns");
+    const countDownTimes = new Date(
+      clockdivs.getAttribute("data-date")
+    ).getTime();
+    
+    const countdownfunctions = setInterval(function () {
+      const nows = new Date().getTime();
+      const diffs = countDownTimes - nows;
+      const dayss = Math.floor(diffs / (1000 * 60 * 60 * 24));
+      const hourss = Math.floor(diffs % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
+      const minutess = Math.floor(diffs % (1000 * 60 * 60) / (1000 * 60));
+      const secondss = Math.floor(diffs % (1000 * 60) / 1000);
+    
+      if (diffs < 0) {
+        clockdivs.style.display = "none";
+        clearInterval(countdownfunctions);
+      } else {
+        clockdivs.querySelector(".dayss").innerHTML = dayss + " :";
+        clockdivs.querySelector(".hourss").innerHTML = hourss + " :";
+        clockdivs.querySelector(".minutess").innerHTML = minutess + " :";
+        clockdivs.querySelector(".secondss").innerHTML = secondss;
+      }
+    }, 1000);
     clockdiv.querySelector(".days").innerHTML = days;
     clockdiv.querySelector(".hours").innerHTML = hours;
     clockdiv.querySelector(".minutes").innerHTML = minutes;
